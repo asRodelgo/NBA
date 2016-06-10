0e13e2f08bf1cce51c85e9cc4e0bfc7db55492b0
Alberto Sanchez <alberto.sanchez.rodelgo@gmail.com>
2016-06-10 08:20:52 -0400

    added overtime tie-breaker

--- data/read_data.R
+++ data/read_data.R
@@ -19,10 +19,13 @@ muA <- teamA$PTS - 3 + teamH$PTSA - global_mean
 pointsH <- round(rnorm(1,muH,sigma),0)
 pointsA <- round(rnorm(1,muA,sigma),0)
 
-while (poinsH-pointsA==0){
-  
+while (pointsH-pointsA==0){ # overtime tie-breaker
+  extraH <- round(rnorm(1,muH*5/48,sigma/3),0)
+  extraA <- round(rnorm(1,muA*5/48,sigma/3),0)
+  pointsH <- pointsH + extraH
+  pointsA <- pointsA + extraA
 }
-print(paste0(team_home,": ",round(rnorm(1,muH,sigma),0)," vs. ",
-             team_away,": ",round(rnorm(1,muA,sigma),0)))
+print(paste0(team_home,": ",pointsH," vs. ",
+             team_away,": ",pointsA))
 
 
