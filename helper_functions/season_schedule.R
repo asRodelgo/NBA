3b766940d2b79ab59bcf8035fc1900e96059dea8
Alberto Sanchez <alberto.sanchez.rodelgo@gmail.com>
2016-07-03 18:58:53 -0400

    season schedule finished

--- helper_functions/season_schedule.R
+++ helper_functions/season_schedule.R
@@ -13,7 +13,8 @@ teamsReordered <- sample(conferences$TeamCode)
 schedule <- matrix(nrow=30,ncol=30,dimnames = list(teamsReordered,teamsReordered))
 
 for (i in 1:29){
-  count3games <- length(schedule[which(schedule[i,]==3)]) # each team plays 3 games against 4 of their fellow conference teams, 4 against the rest
+  count3games <- length(schedule[which(schedule[i,]+schedule[,i]==3)]) # each team plays 3 games against 4 of their fellow conference teams, 4 against the rest
+  limit_home <- 0 # Out of those 3 games, 1 has to be away twice, and 2 away twice
   for (j in (i+1):30){
     thisTeam <- dimnames(schedule)[[1]][i]
     vsTeam <- dimnames(schedule)[[2]][j]
@@ -21,18 +22,66 @@ for (i in 1:29){
     vsConf <- filter(conferences, TeamCode == vsTeam)$Conference
     thisConfTeams <- filter(conferences, Conference == thisConf)$TeamCode
     if (!(thisConf == vsConf)){
+      schedule[i,j] <- 1
+      schedule[j,i] <- 1
+    } else if (count3games < 4) {
+      if (limit_home < 2) {
+        schedule[i,j] <- 2
+        schedule[j,i] <- 1
+        limit_home <- limit_home + 1
+      } else {
+        schedule[i,j] <- 1
+        schedule[j,i] <- 2
+      }
+      count3games <- count3games + 1
+    } else {
       schedule[i,j] <- 2
       schedule[j,i] <- 2
-    } else if (count3games<4) {
-        schedule[i,j] <- 3
-        schedule[j,i] <- 3
-        count3games <- count3games + 1
-    } else {
-      schedule[i,j] <- 4
-      schedule[j,i] <- 4
     }
   }
 }
 
 # set up the schedule for the regular season. On average there are 6 games per day
+gamesUrn <- data.frame()
+
+urn_count <- 1
+for (i in 1:30){
+  for (j in 1:30){
+    if (!(i==j)){
+      if (schedule[i,j]==2){
+        gamesUrn[urn_count,1] <- i
+        gamesUrn[urn_count,2] <- j
+        urn_count <- urn_count + 1
+        gamesUrn[urn_count,1] <- i
+        gamesUrn[urn_count,2] <- j
+        urn_count <- urn_count + 1
+      } else {
+        gamesUrn[urn_count,1] <- i
+        gamesUrn[urn_count,2] <- j
+        urn_count <- urn_count + 1
+      }
+    }
+  }
+}
+
+season <- data.frame()
+set.seed(23)
+gamesUrnSamp <- sample_frac(gamesUrn)
+
+day <- 1
+day_count <- 0
+for (i in 1:nrow(gamesUrnSamp)){
+  if (day_count>6){
+    day_count <- 0
+    day <- day + 1
+  }  
+
+  season[i,1] <- day
+  season[i,2] <- dimnames(schedule)[[1]][gamesUrnSamp[i,1]]
+  season[i,3] <- dimnames(schedule)[[1]][gamesUrnSamp[i,2]]
+  
+  day_count <- day_count + 1
+  
+}
+
 
