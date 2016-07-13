a678eafb3aae8f31510896172b91eb77a9573ca0
Alberto Sanchez <alberto.sanchez.rodelgo@gmail.com>
2016-07-12 20:41:02 -0400

    Start neural network computation

---
+++ helper_functions/neural_network.R
@@ -0,0 +1,92 @@
+# Compute a model that uses players skills in a team as input and points scored by the team
+# as output.
+# Compute a second model that uses the same inputs and the team plus_minus as output
+#
+# My initial approach will be a neural network with #players as input units and 1 output unit
+# or 2 output units if combining the 2 models above makes sense.
+#
+# Reference: http://www.r-bloggers.com/fitting-a-neural-network-in-r-neuralnet-package/
+library(dplyr)
+library(caret)
+library(doMC) # use parallel processing on this machine through "foreach"
+registerDoMC(2) # As far as I know my MAC works on 2 cores
+library(neuralnet) # neural network for regression
+
+# DATA PROCESSING --------------------------------------------
+
+# Approach: Summarize variables at team level to obtain input vector for the model
+# 1. By team: calculate weighted average of each characteristic: FGM, FGA, etc...
+data_team <- .team_prepare() # If no arguments, will calculate for all teams for last season
+# 2. Weights correspond to percentage of total team time played, ie, sum(effMin) = 5
+# Probably more efficient to scale weights to add up to 1: Wt = effMin/5
+playersSumm <- data_team %>%
+  filter(!(Tm == "TOT")) %>% # Those who played for more than 1 team have a Total team
+  group_by(Tm) %>%
+  mutate(Wt = effMin/5) %>%
+  mutate_each(funs(weighted.mean(.,Wt)),-Player,-Pos,-Season,-Wt) %>%
+  dplyr::select(-Player,-Pos,-Wt) %>%
+  distinct(.keep_all=TRUE)
+
+playersSumm <- as.data.frame(playersSumm)
+playersSumm <- playersSumm[, !(names(playersSumm) %in% c("Player","Wt"))]
+
+# add team's average points in season (output variable y~ in the regression)
+playersSumm <- merge(playersSumm, teams[,c("TeamCode","TEAM_PTS")], by.x = "TEAM", by.y = "TeamCode")
+
+# scale the data for easier convergence of backpropagation algorithm
+maxs <- apply(playersSumm[,-1], 2, max) 
+mins <- apply(playersSumm[,-1], 2, min)
+
+teamCodes <- playersSumm[,1]
+scaled <- as.data.frame(scale(playersSumm[,-1], center = mins, scale = maxs - mins))
+scaled <- cbind(teamCodes,scaled)
+
+# CROSS VALIDATION --------------------------------------------
+
+# k: number of splits train-test
+# train_split: number of teams or percentage of data in training set
+set.seed(450)
+cv.error <- NULL
+k <- 10
+train_split <- 24
+hidden_neurons <- c(6,4,2)
+#c(4,2)
+#c(6,4,2)
+# neuralnet requires explicit formula for the model (f)
+n <- names(training)
+f <- as.formula(paste("TEAM_PTS ~", paste(n[!n %in% "TEAM_PTS"], collapse = " + ")))
+
+for(i in 1:k){
+  teams_train <- sample(teams$TeamCode,train_split)
+  teams_test <- filter(teams, !(TeamCode %in% teams_train))$TeamCode
+  training <- filter(scaled, teamCodes %in% teams_train)
+  testing <- filter(scaled, teamCodes %in% teams_test)
+  
+  # remove non-numeric variables
+  train_teamCodes <- training$teamCodes
+  test_teamCodes <- testing$teamCodes
+  training <- training[,-1]
+  testing <- testing[,-1]
+  
+  ## Model Neural Network
+  # Hidden layers and neurons per layer specified by hidden. 
+  # Number of input neurons is the number of columns
+  # Output neurons is 1 as we are doing regression (linear.output=T)
+  # For classification problem, linear.output=F
+  nn <- neuralnet(f,data=training,hidden=hidden_neurons,linear.output=T)
+  
+  # Prediction
+  pr.nn <- compute(nn,testing[,-ncol(testing)])
+  # Model results are scaled so need to scale them back to normal
+  pr.nn_ <- pr.nn$net.result*(max(playersSumm$TEAM_PTS)-min(playersSumm$TEAM_PTS))+min(playersSumm$TEAM_PTS)
+  test.r <- (testing$TEAM_PTS)*(max(playersSumm$TEAM_PTS)-min(playersSumm$TEAM_PTS))+min(playersSumm$TEAM_PTS)
+  
+  cv.error[i] <- sum((test.r - pr.nn_)^2)/nrow(testing)
+  
+}
+
+# plot MSE distribution after C-V
+boxplot(cv.error,xlab='MSE CV',col='cyan',
+        border='blue',names='CV error (MSE)',
+        main='CV error (MSE) for NN',horizontal=TRUE)
+
--- helper_functions/teamStats.R
+++ helper_functions/teamStats.R
@@ -4,37 +4,70 @@
 #thisTeamPlayers <- as.character(filter(playersHist, Tm == thisTeam, Season == max(as.character(Season)))$Player)
 
 # compute effective stats
-.team_prepare <- function(thisTeam){
+.team_prepare <- function(thisTeam="All"){
   
-  data_team <- playersHist %>%
-    filter(Tm == thisTeam, Season == max(as.character(Season))) %>%
-    group_by(Player) %>%
-    mutate(effMin = MP/3936, effFG = FG/(3936*effMin),
-           effFGA = FGA/(3936*effMin),eff3PM = X3P/(3936*effMin),eff3PA = X3PA/(3936*effMin),
-           eff2PM = X2P/(3936*effMin),eff2PA = X2PA/(3936*effMin),
-           effFTM = FT/(3936*effMin),effFTA = FTA/(3936*effMin),
-           effORB = ORB/(3936*effMin),effDRB = DRB/(3936*effMin),
-           effTRB = TRB/(3936*effMin),effAST = AST/(3936*effMin),
-           effSTL = STL/(3936*effMin),effBLK = BLK/(3936*effMin),
-           effTOV = TOV/(3936*effMin),effPF = PF/(3936*effMin),
-           effPTS = PTS/(3936*effMin)) %>%
-    select(Player,Pos,Season,Age,FGPer = FG.,FG3Per = X3P., FG2Per = X2P., effFGPer = eFG.,
-           FTPer = FT., starts_with("eff"),
-           -Tm,-G,-GS,-MP,FG,-FGA,-X3P,-X3PA,-X2P,-X2PA,-FG,-FTA,-ORB,-DRB,-TRB,-AST,
-           -BLK,-TOV,-PF,-FT,-STL,-PTS)
-  
-  # Impute NAs by 0. If NA means no shot attempted, ie, 
-  # either the player didn't play enough time or is really bad at this particular type of shot.
-  for (i in 4:ncol(data_team)){
-    data_team[is.na(data_team[,i]),i] <- 0
+  if (thisTeam == "All"){
+    
+    data_team <- playersHist %>%
+      filter(Season == max(as.character(Season))) %>%
+      #group_by(Player) %>%
+      mutate(effMin = MP/3936, effFG = FG/(3936*effMin),
+             effFGA = FGA/(3936*effMin),eff3PM = X3P/(3936*effMin),eff3PA = X3PA/(3936*effMin),
+             eff2PM = X2P/(3936*effMin),eff2PA = X2PA/(3936*effMin),
+             effFTM = FT/(3936*effMin),effFTA = FTA/(3936*effMin),
+             effORB = ORB/(3936*effMin),effDRB = DRB/(3936*effMin),
+             effTRB = TRB/(3936*effMin),effAST = AST/(3936*effMin),
+             effSTL = STL/(3936*effMin),effBLK = BLK/(3936*effMin),
+             effTOV = TOV/(3936*effMin),effPF = PF/(3936*effMin),
+             effPTS = PTS/(3936*effMin)) %>%
+      dplyr::select(Player,Pos,Season,Tm,Age,FGPer = FG.,FG3Per = X3P., FG2Per = X2P., effFGPer = eFG.,
+             FTPer = FT., starts_with("eff"),
+             -G,-GS,-MP,FG,-FGA,-X3P,-X3PA,-X2P,-X2PA,-FG,-FTA,-ORB,-DRB,-TRB,-AST,
+             -BLK,-TOV,-PF,-FT,-STL,-PTS)
+    
+    # Impute NAs by 0. If NA means no shot attempted, ie, 
+    # either the player didn't play enough time or is really bad at this particular type of shot.
+    for (i in 5:ncol(data_team)){
+      data_team[is.na(data_team[,i]),i] <- 0
+    }
+    # Adjust effMin to reflect percentage of total team time played, ie, sum(effMin) = 5
+    data_team <- data_team %>%
+      group_by(Tm) %>%
+      mutate(effMin = 5*effMin/sum(effMin,na.rm=TRUE))
+    
+    data_team <- as.data.frame(data_team)
+    
+  } else{
+    data_team <- playersHist %>%
+      filter(Tm == thisTeam, Season == max(as.character(Season))) %>%
+      group_by(Player) %>%
+      mutate(effMin = MP/3936, effFG = FG/(3936*effMin),
+             effFGA = FGA/(3936*effMin),eff3PM = X3P/(3936*effMin),eff3PA = X3PA/(3936*effMin),
+             eff2PM = X2P/(3936*effMin),eff2PA = X2PA/(3936*effMin),
+             effFTM = FT/(3936*effMin),effFTA = FTA/(3936*effMin),
+             effORB = ORB/(3936*effMin),effDRB = DRB/(3936*effMin),
+             effTRB = TRB/(3936*effMin),effAST = AST/(3936*effMin),
+             effSTL = STL/(3936*effMin),effBLK = BLK/(3936*effMin),
+             effTOV = TOV/(3936*effMin),effPF = PF/(3936*effMin),
+             effPTS = PTS/(3936*effMin)) %>%
+      select(Player,Pos,Season,Age,FGPer = FG.,FG3Per = X3P., FG2Per = X2P., effFGPer = eFG.,
+             FTPer = FT., starts_with("eff"),
+             -Tm,-G,-GS,-MP,FG,-FGA,-X3P,-X3PA,-X2P,-X2PA,-FG,-FTA,-ORB,-DRB,-TRB,-AST,
+             -BLK,-TOV,-PF,-FT,-STL,-PTS)
+    
+    # Impute NAs by 0. If NA means no shot attempted, ie, 
+    # either the player didn't play enough time or is really bad at this particular type of shot.
+    for (i in 4:ncol(data_team)){
+      data_team[is.na(data_team[,i]),i] <- 0
+    }
+    # Adjust effMin to reflect percentage of total team time played, ie, sum(effMin) = 5
+    teamMinutes <- sum(data_team$effMin)
+    data_team$effMin <- 5*data_team$effMin/teamMinutes
+    
+    data_team <- as.data.frame(data_team)
+    
   }
-  # Adjust effMin to reflect percentage of total team time played, ie, sum(effMin) = 5
-  teamMinutes <- sum(data_team$effMin)
-  data_team$effMin <- 5*data_team$effMin/teamMinutes
-  
-  data_team <- as.data.frame(data_team)
   return(data_team)
-  
 }
 
 # swap 2 players (transfer)
