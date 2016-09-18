1f8ca0b89fe41e514f9afee53232024517584c41
Alberto Sanchez <alberto.sanchez.rodelgo@gmail.com>
2016-09-18 07:19:50 -0400

    aligned selectors tsne

--- ui_files/ui_plotTSNE.R
+++ ui_files/ui_plotTSNE.R
@@ -1,13 +1,15 @@
 # plot TSNE UI ----------
 
 fluidPage(
-  column(12,
-         selectInput('colSeason', 'Season:', choices=c("All",sort(unique(data_tsne_sample$Season))),selectize=FALSE),
-         selectInput('colTeam', 'Team:', choices=c("All",sort(unique(data_tsne_sample$Tm))),selectize=FALSE),
-         selectInput('colPlayer', 'Player:', choices=c("All",sort(unique(data_tsne_sample$Player))),selectize=FALSE),
-         selectInput('colSkill', 'Skill:', choices=c("All",names(data_tsne_sample)[6:ncol(data_tsne_sample)]),selectize=FALSE),
-         selectInput('colAge', 'Age:', choices=c("All",sort(unique(data_tsne_sample$Age))),selectize=FALSE),
-         br(),
+  fluidRow(splitLayout(cellWidths = rep("18%", 5),
+              selectInput('colSeason', 'Season:', choices=c("All",sort(unique(data_tsne_sample$Season))),selectize=FALSE),
+              selectInput('colTeam', 'Team:', choices=c("All",sort(unique(data_tsne_sample$Tm))),selectize=FALSE),
+              selectInput('colPlayer', 'Player:', choices=c("All",sort(unique(data_tsne_sample$Player))),selectize=FALSE),
+              selectInput('colSkill', 'Skill:', choices=c("All",names(data_tsne_sample)[6:ncol(data_tsne_sample)]),selectize=FALSE),
+              selectInput('colAge', 'Age:', choices=c("All",sort(unique(data_tsne_sample$Age))),selectize=FALSE)
+          )
+  ),        
+  br(),
          div(
            style = "position:relative",
            plotOutput('plotTSNE', 
@@ -15,7 +17,6 @@ fluidPage(
            uiOutput("hover_info")
          )
   )
-)
 
 
 
