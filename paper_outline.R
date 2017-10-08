d6c35371f074e2dc684d7957e88fe21df19f23f4
Alberto Sanchez <alberto.sanchez.rodelgo@gmail.com>
2017-10-08 15:29:08 -0400

    computed full season simulation with predicted team powers

--- paper_outline.R
+++ paper_outline.R
@@ -47,6 +47,14 @@ conferences <- read.csv("data/nba_conferences.csv", stringsAsFactors = FALSE) # 
 nn_Offense <- list.load("data/nn_Offense.rds")
 nn_Defense <- list.load("data/nn_Defense.rds")
 
+# Actual Season schedule
+realSeasonSchedule <- read.csv("data/realSeasonSchedule.csv",stringsAsFactors = FALSE) # from write_seasonSchedule.R
+datesRange <- unique(realSeasonSchedule$Date)
+
+# Global hyperparameters for Normal distributions
+global_mean <- mean(team_stats$PTS)
+sigma <- 8 # constant std dev for all teams. ADJUST LATER ON!!
+
 
 ########################################
 # 1. calculate playersNewPredicted
@@ -180,7 +188,43 @@ playersNewPredicted_Final_adjMin2 <- mutate(playersNewPredicted_Final_adjMin2, S
 # compute team powers
 effMinutes <- NULL # approx the average of all 
 teamPowers_newSeason <- merge(.computePower(playersNewPredicted_Final_adjMin2,"PTS","All",effMinutes,actualOrPredicted = "predicted"),.computePower(playersNewPredicted_Final_adjMin2,"PTSA","All",effMinutes,actualOrPredicted = "predicted"),by="team_season")
+teamsPredicted <- .teamsPredictedPower(data = playersNewPredicted_Final_adjMin2,actualOrPred="predicted")
 # First analysis: OKC and BOS defenses are awful. Looks like they have great starting lineups but crappy benches
 # The rest of the teams get more or less expected predictions
 
+# Simulate a few seasons
+regSeasonOutcome <- .standings(real = TRUE)
+# Initialize parameters
+regSeasonAvg2 <- data.frame(
+  team = regSeasonOutcome[[1]][[168]]$team,
+  teamCode = regSeasonOutcome[[1]][[168]]$teamCode,
+  conference = regSeasonOutcome[[1]][[168]]$conference,
+  win = 0,
+  lose = 0,
+  win2 = 0,
+  sd = 0,
+  probChamp = 0)
+
+num_seasons <- 100
+
+for (i in 1:num_seasons){
+  
+  final_standings <- regSeasonOutcome[[1]][[168]]
+  #playoffs <- .getPlayoffResults(final_standings) %>% mutate(round = ifelse(round == 0,1,0)) %>%
+  #  group_by(teamCode) %>% summarise(round = sum(round)) %>% ungroup()
+  regSeasonAvg2$win <- regSeasonAvg2$win + final_standings$win
+  regSeasonAvg2$win2 <- regSeasonAvg2$win2 + (final_standings$win)^2
+  #probChamp <- merge(final_standings, playoffs[,c("teamCode","round")],by="teamCode",all.x=TRUE) %>%
+  #  mutate(round = ifelse(is.na(round),0,round))
+  #regSeasonAvg2$probChamp <- regSeasonAvg2$probChamp + probChamp$round
+  # generate a new season outcome
+  regSeasonOutcome <- .standings(real = TRUE)
+  # keep count
+  print(paste0("iteration: ",i))
+}
+
+regSeasonAvg2$win <- regSeasonAvg2$win/num_seasons
+regSeasonAvg2$lose <- 82 - regSeasonAvg2$win
+regSeasonAvg2$win2 <- regSeasonAvg2$win2/num_seasons
+regSeasonAvg2$sd <- sqrt(regSeasonAvg2$win2 - (regSeasonAvg2$win)^2)
 
