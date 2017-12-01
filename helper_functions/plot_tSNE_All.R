f59964b044423a42fe3cb85680365d718b9e2827
Alberto Sanchez <alberto.sanchez.rodelgo@gmail.com>
2017-12-01 11:45:12 -0500

    Added t-SNE historical plot

--- helper_functions/plot_tSNE_All.R
+++ helper_functions/plot_tSNE_All.R
@@ -1,5 +1,5 @@
 # Plot tsne chart ---------------------------------------------------------
-.tSNE_plot_All <- function(colTeam,colSeason,colPlayer,colAge,colSkill){
+.tSNE_plot_All <- function(data = tsne_ready,colTeam,colSeason,colPlayer,colAge,colSkill,num_clusters = 10){
   # tsne_points contains pairs of coordinate points to plot
   # Parameters -----------
   # 
@@ -11,74 +11,142 @@
   # ----------------------
   #
   # ------------------------------------
-  if (colPlayer=="All") colPlayer <- players_list
-  if (colAge=="All") colAge <- ages_list
-  if (colTeam=="All") colTeam <- teams_list
-  if (colSeason=="All") colSeason <- seasons_list
-  #
-  if (length(tsne_ready)>0){ # if data do stuff
+  # Check whether the user has selected any input
+  color_details <- FALSE
+  
+  if (colPlayer=="All") colPlayer <- players_list else color_details <- TRUE
+  if (colAge=="All") colAge <- ages_list else color_details <- TRUE
+  if (colTeam=="All") colTeam <- teams_list else color_details <- TRUE
+  if (colSeason=="All") colSeason <- seasons_list else color_details <- TRUE
+  # Add clusters
+  set.seed(456)
+  playerCluster <- kmeans(data[, c("x","y")], num_clusters, nstart = 10, iter.max = 20)
+  data <- cbind(data, cluster = playerCluster$cluster)
+  
+  #centroid <- data.frame(x=(mean(tsne_points_filter$x)),y=mean(tsne_points_filter$y))
+  #x_limit <- c(min(tsne_points_filter$x)-5,max(tsne_points_filter$x)+10)
+  #y_limit <- c(min(tsne_points_filter$y)-5,max(tsne_points_filter$y)+10)
+  
+  cluster_representative <- group_by(data, cluster) %>%
+    mutate(x_mean = mean(x), y_mean=mean(y)) %>%
+    mutate(dist = sqrt((x-x_mean)^2+(y-y_mean)^2)) %>%
+    filter(dist==min(dist)) %>%
+    select(cluster, Player,Season, x,y,dist) %>%
+    ungroup()
+  
+  if (length(data)>0){ # if data do stuff
     par(mar=c(0,0,0,0))
     
-    tsne_ready_plot <- tsne_ready %>% # by default all colored grey
+    tsne_ready_plot <- data %>% # by default all colored grey
       mutate(color = "lightgrey", colorDots = "grey")
    
     # General Filters
     tsne_points_filter <- tsne_ready_plot %>%
       filter(Player %in% colPlayer & Age %in% colAge
              & Tm %in% colTeam & Season %in% colSeason)
+      
     centroid <- data.frame(x=(mean(tsne_points_filter$x)),y=mean(tsne_points_filter$y))
     
     tsne_points_filter_out <- tsne_ready_plot %>%
       filter(!(Player %in% colPlayer & Age %in% colAge
              & Tm %in% colTeam & Season %in% colSeason))
     # Skills filter
-    if (!(colSkill=="All")){
       
-      ggplot(NULL, aes(x,y)) +
-        geom_point(data=tsne_points_filter,aes(color = eval(parse(text=colSkill))),size=2) +
-        scale_color_gradient2(midpoint=mean(eval(parse(text=paste0("tsne_points_filter$",colSkill)))), low="blue", mid="white",high="red")+
-        geom_point(data=tsne_points_filter_out,color=alpha("lightgrey",0.1)) + 
-        theme(legend.key=element_blank(),
-              legend.title=element_blank(),
-              legend.text = element_blank(),
-              panel.border = element_blank(),
-              panel.background = element_blank(),
-              axis.text.x = element_blank(),
-              axis.text.y = element_blank(),
-              axis.title.x = element_blank(),
-              axis.title.y = element_blank(),
-              axis.ticks = element_blank())
-
-    } else {
-      
-        starPlayers <- c("LeBron James","Russell Westbrook","Kawhi Leonard","Stephen Curry",
-                         "Draymond Green","Kevin Durant","Anthony Davis",
-                         "James Harden","Klay Thompson","DeMarcus Cousins",
-                         "Chris Paul","John Wall","Paul George",
-                         "Marc Gasol","JaVale McGee","Andre Drummond",
-                         "Kelly Oubre","Ryan Anderson","Kelly Olynyk","Channing Frye",
-                         "Brandon Jennings","Tony Allen","Tristan Thompson","Ian Mahinmi",
-                         "Dennis Schroder","Justin Harper","Luis Scola")
-        labelsPlayers <- filter(tsne_ready_plot, Player %in% starPlayers, Season == "2016-2017")
-        ggplot(NULL, aes(x,y)) +  
-        geom_point(data=tsne_points_filter,color = "red",size=4) +
-        geom_text(data=tsne_points_filter,aes(label = paste0(Player," (",Season,")"))) +
-        geom_text(data=labelsPlayers,aes(label = Player,
-                                                group = Player,color = Player)) +  
-        geom_point(data=tsne_points_filter_out,color=alpha("lightgrey",0.1)) +
-        #geom_point(data=centroid,color="red",size=3) + 
-        theme(#legend.key=element_blank(),
-              #legend.title=element_blank(),
-              #legend.text = element_blank(),
-              legend.position = "top",
-              panel.border = element_blank(),
-              panel.background = element_blank(),
-              axis.text.x = element_blank(),
-              axis.text.y = element_blank(),
-              axis.title.x = element_blank(),
-              axis.title.y = element_blank(),
-              axis.ticks = element_blank())
-    }
+    #if (color_details) {
+      if (colSkill == "K-means" & color_details) { # color by K-means clusters
+        ggplot(NULL, aes(x,y)) +
+          geom_point(data=data,aes(color = as.factor(cluster)),alpha = 0.2) +
+          geom_jitter(data=tsne_points_filter,color = "red",size=4,width = 1.5, height = 1.5) +
+          geom_text(data = cluster_representative, aes(label = paste0(Player," (",Season,")")),color = "grey",size=3, nudge_y = 1) +
+          geom_text(data=tsne_points_filter,aes(label = Player),color="blue",size=3, nudge_y = 1) +
+          theme(legend.key=element_blank(),
+                legend.title=element_blank(),
+                legend.text = element_blank(),
+                panel.border = element_blank(),
+                panel.background = element_blank(),
+                axis.text.x = element_blank(),
+                axis.text.y = element_blank(),
+                axis.title.x = element_blank(),
+                axis.title.y = element_blank(),
+                axis.ticks = element_blank())
+      } else if (colSkill == "K-means"){
+        ggplot(NULL, aes(x,y)) +
+          geom_point(data=data,aes(color = as.factor(cluster)),alpha = 0.2) +
+          geom_text(data = cluster_representative, aes(label = paste0(Player," (",Season,")")),color = "grey",size=3, nudge_y = 1) +
+          #scale_color_gradient2(midpoint=mean(eval(parse(text=paste0("tsne_points_filter$",colSkill)))), low="blue", mid="white",high="red")+
+          #geom_point(data=tsne_points_filter_out,color=alpha("lightgrey",0.1)) + 
+          theme(legend.key=element_blank(),
+                legend.title=element_blank(),
+                legend.text = element_blank(),
+                panel.border = element_blank(),
+                panel.background = element_blank(),
+                axis.text.x = element_blank(),
+                axis.text.y = element_blank(),
+                axis.title.x = element_blank(),
+                axis.title.y = element_blank(),
+                axis.ticks = element_blank())
+      } else {
+        ggplot(NULL, aes(x,y)) +
+          geom_point(data=tsne_points_filter,aes(color = eval(parse(text=colSkill))),size=2) +
+          scale_color_gradient2(midpoint=mean(eval(parse(text=paste0("tsne_points_filter$",colSkill)))), low="blue", mid="white",high="red")+
+          geom_point(data=tsne_points_filter_out,color=alpha("lightgrey",0.1)) + 
+          theme(legend.key=element_blank(),
+                legend.title=element_blank(),
+                legend.text = element_blank(),
+                panel.border = element_blank(),
+                panel.background = element_blank(),
+                axis.text.x = element_blank(),
+                axis.text.y = element_blank(),
+                axis.title.x = element_blank(),
+                axis.title.y = element_blank(),
+                axis.ticks = element_blank())
+      }
+    # } else {
+    #   ggplot(NULL, aes(x,y)) +
+    #     geom_point(data=tsne_points_filter,aes(color = eval(parse(text=colSkill))),size=2) +
+    #     scale_color_gradient2(midpoint=mean(eval(parse(text=paste0("tsne_points_filter$",colSkill)))), low="blue", mid="white",high="red")+
+    #     geom_point(data=tsne_points_filter_out,color=alpha("lightgrey",0.1)) + 
+    #     theme(legend.key=element_blank(),
+    #           legend.title=element_blank(),
+    #           legend.text = element_blank(),
+    #           panel.border = element_blank(),
+    #           panel.background = element_blank(),
+    #           axis.text.x = element_blank(),
+    #           axis.text.y = element_blank(),
+    #           axis.title.x = element_blank(),
+    #           axis.title.y = element_blank(),
+    #           axis.ticks = element_blank())
+    # }
+    # } else {
+    #   
+    #     # starPlayers <- c("LeBron James","Russell Westbrook","Kawhi Leonard","Stephen Curry",
+    #     #                  "Draymond Green","Kevin Durant","Anthony Davis",
+    #     #                  "James Harden","Klay Thompson","DeMarcus Cousins",
+    #     #                  "Chris Paul","John Wall","Paul George",
+    #     #                  "Marc Gasol","JaVale McGee","Andre Drummond",
+    #     #                  "Kelly Oubre","Ryan Anderson","Kelly Olynyk","Channing Frye",
+    #     #                  "Brandon Jennings","Tony Allen","Tristan Thompson","Ian Mahinmi",
+    #     #                  "Dennis Schroder","Justin Harper","Luis Scola")
+    #     #labelsPlayers <- filter(tsne_ready_plot, Player %in% starPlayers, Season == "2016-2017")
+    #     ggplot(NULL, aes(x,y)) +  
+    #     geom_point(data=tsne_points_filter,color = "blue",size=4) +
+    #     geom_text(data=tsne_points_filter,aes(label = paste0(Player," (",Season,")"))) +
+    #     geom_text(data = cluster_representative, aes(label = paste0(Player," (",Season,")")),color = "grey",size=3, nudge_y = 1) +
+    #     #geom_text(data=labelsPlayers,aes(label = Player,group = Player,color = Player)) +  
+    #     geom_point(data=tsne_points_filter_out,color=alpha("lightgrey",0.1)) +
+    #     #geom_point(data=centroid,color="red",size=3) + 
+    #     theme(legend.key=element_blank(),
+    #           legend.title=element_blank(),
+    #           legend.text = element_blank(),
+    #           legend.position = "top",
+    #           panel.border = element_blank(),
+    #           panel.background = element_blank(),
+    #           axis.text.x = element_blank(),
+    #           axis.text.y = element_blank(),
+    #           axis.title.x = element_blank(),
+    #           axis.title.y = element_blank(),
+    #           axis.ticks = element_blank())
+    # }
     
     #plot(tsne_points,type = "p", pch = 19, axes=FALSE, frame.plot = FALSE, xlab = "",ylab = "",col = tsne_ready$colorDots); 
 #    graphics::text(tsne_points_filter[,c("x","y")],
@@ -92,19 +160,19 @@
 }
 
 # Plot tsne chart ---------------------------------------------------------
-.tSNE_plot_filter <- function(colTeam,colSeason,colPlayer,colAge,colSkill){
+.tSNE_plot_filter <- function(data = tsne_ready, colTeam,colSeason,colPlayer,colAge,colSkill){
   #
   if (colPlayer=="All") colPlayer <- players_list
   if (colAge=="All") colAge <- ages_list
   if (colTeam=="All") colTeam <- teams_list
   if (colSeason=="All") colSeason <- seasons_list
   
-  if (length(tsne_ready)>0){ # if data do stuff
+  if (length(data)>0){ # if data do stuff
     # General Filters
-    tsne_points_filter <- tsne_ready %>%
+    tsne_points_filter <- data %>%
       filter(Player %in% colPlayer & Age %in% colAge
              & Tm %in% colTeam & Season %in% colSeason)
-    tsne_points_filter_out <- tsne_ready %>%
+    tsne_points_filter_out <- data %>%
       filter(!(Player %in% colPlayer & Age %in% colAge
                & Tm %in% colTeam & Season %in% colSeason))
     
